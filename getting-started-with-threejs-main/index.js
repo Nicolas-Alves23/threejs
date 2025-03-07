e7fc561889fbbf226738e09919e5b5b95dac2863
nicolasalvesss <nicolasvilelabarros@gmail.com>
2025-03-07 15:32:32 -0300

    minha bolinha

--- getting-started-with-threejs-main/index.js
+++ getting-started-with-threejs-main/index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js"
 // inicializando o height e o width 
 const w = window.innerWidth
 const h = window.innerHeight
@@ -11,8 +12,13 @@ const aspect = w / h
 const near = 0.2
 const far = 10
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-camera.position.z = 2
+camera.position.z = 5
 const scene = new THREE.Scene()
+
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.dampingFactor = 0.03
 
 const geo = new THREE.IcosahedronGeometry(1.0, 2)
 const mat = new THREE.MeshStandardMaterial({
@@ -24,20 +30,21 @@ const mesh = new THREE.Mesh(geo,mat)
 scene.add(mesh)
 
 const wireMat = new THREE.MeshBasicMaterial({
-    color: '#000000',
+    color: '#ffffff',
     wireframe: true
 })
 
 const wireMesh = new THREE.Mesh(geo, wireMat)
 wireMesh.scale.setScalar(1.002)
-scene.add(wireMesh)
+mesh.add(wireMesh)
 
-const hemilight = new THREE.HemisphereLight(0xffffff, 0x000000)
+const hemilight = new THREE.HemisphereLight(0xffffff, "#2FED39")
 scene.add(hemilight) 
 
 function animate(t = 0){
   requestAnimationFrame(animate)
   mesh.rotation.y = t * 0.0001
   renderer.render(scene,camera)
+  controls.update()
 }
 animate()
